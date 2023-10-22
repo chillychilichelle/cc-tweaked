5d0b4c1ef74f034db52cb1da81d9b84d1ea7fb50
Chilly Chili <chillybones08@hotmail.com>
2023-10-22 22:41:21 +0800

    testing spiral moving back upwards

--- spiral.lua
+++ spiral.lua
@@ -1,9 +1,20 @@
 turtle.refuel()
-while turtle.detectDown() do
+local starting = turtle.getFuelLevel()
+local half = starting/2
+local count = 0
+while (turtle.detectDown() and turtle.getFuelLevel()>half) do
     turtle.dig()
     turtle.digDown()
     turtle.down()
     turtle.dig()
     turtle.forward()
     turtle.turnLeft()
+    count = count + 1
+end
+
+for i = count,1,-1
+do
+    turtle.turnRight()
+    turtle.back()
+    turtle.up()
 end
