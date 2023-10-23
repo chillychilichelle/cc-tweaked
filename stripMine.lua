4596710bb40e974bc42ebcb9a063804ac8388ab7
Chilly Chili <chillybones08@hotmail.com>
2023-10-24 02:29:36 +0800

    strip mining

--- stripMine.lua
+++ stripMine.lua
@@ -3,14 +3,13 @@ local starting = turtle.getFuelLevel()
 local half = starting/2
 local count = 0
 while (turtle.getFuelLevel()>half) do
-    while turtle.detect() do
-        turtle.dig()        
-    end
     turtle.up()
+    turtle.dig()        
+    
+    turtle.down()
     while turtle.detect() do
         turtle.dig()        
     end
-    turtle.down()
     turtle.forward()
 
     count = count + 1
