89fbd73dfe1f1d3b10beca847818c253e4dac103
Chilly Chili <chillybones08@hotmail.com>
2023-10-23 20:16:16 +0800

    testing builder + rn

--- builder.lua
+++ builder.lua
@@ -12,6 +12,23 @@ local file = fs.open(target,"r")
 local x = tonumber(file.readLine())
 local y = tonumber(file.readLine())
 local z = tonumber(file.readLine())
-print("X: "..x.." Y: "..y.." Z: "..z)
+print("X:  "..x.." Y:  "..y.." Z:  "..z)
+
+local mapData ={}
+for i = 1, z, 1 do
+    mapData[i]={}
+    for j = 1, y, 1 do
+        local str = file.readLine()
+        mapData[i][j] = str;
+    end
+    local _ =file.readLine()
+end
 
 file.close()
+for i = 1, z, 1 do
+    for j = 1, y, 1 do
+        print(mapData[i][j])
+        
+    end
+    
+end
