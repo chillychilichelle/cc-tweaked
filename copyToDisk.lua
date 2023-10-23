75b28b18ff9b472158b277ded22068938cfd1cb6
Chilly Chili <chillybones08@hotmail.com>
2023-10-23 16:54:14 +0800

    fixed wildcard search

--- copyToDisk.lua
+++ copyToDisk.lua
@@ -2,6 +2,11 @@ local files = fs.find("*.lua")
 
 for i = 1, #files, 1 do
     print(files[i])
-    fs.copy(files[i], "disk/"..files[i])
+    local newName = "disk/"..files[i]
+    if(fs.exists(newName)) then
+        fs.delete(newName)
+    end
+
+    fs.copy(files[i], newName)
 
 end
