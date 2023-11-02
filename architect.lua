ba56dbc2291fb5daf636679db52f453b8f162a63
Chilly Chili <chillychilichelle@gmail.com>
2023-11-03 03:42:20 +0800

    architect test with full mob spawner

--- architect.lua
+++ architect.lua
@@ -23,6 +23,7 @@ local z = tonumber(file.readLine())
 print("X:  "..x.." Y:  "..y.." Z:  "..z)
 
 local mapData ={}
+local blockCnt=0
 
 for i = 1, z, 1 do
     local mapDataZCnt = 1
@@ -36,6 +37,7 @@ for i = 1, z, 1 do
                 mapData[i][mapDataZCnt][1]=k
                 mapData[i][mapDataZCnt][2]=j
                 mapDataZCnt = mapDataZCnt + 1
+                blockCnt = blockCnt + 1
             end
         end
     end
@@ -44,14 +46,16 @@ end
 
 file.close()
 
+print("This blueprint requires "..blockCnt.." blocks to construct!")
 
 local currentCoords = {1, 1}
 
--- for each z layer
+
+--nearest neighbour algorithm per z-layer
 for i = 1, z, 1 do
     local layersPoints = mapData[i]
 
-    print("Layer has "..#layersPoints.." points")
+    --print("Layer has "..#layersPoints.." points")
 
     while #layersPoints > 0 do
         local nn = layersPoints[1]
@@ -61,19 +65,19 @@ for i = 1, z, 1 do
         for j=1, #layersPoints, 1 do
             local newCoords= layersPoints[j]
             local newDist = manhattan(currentCoords, newCoords)
-            print("Checking "..newCoords[1]..", "..newCoords[2])
+            --print("Checking "..newCoords[1]..", "..newCoords[2])
             if (newDist <nnDist) then
                 nn = newCoords
                 nnDist = newDist
                 nnInd = j
-                print("New nearest neighbour is "..nn[1]..", "..nn[2])
+                --print("New nearest neighbour is "..nn[1]..", "..nn[2])
             end
-            if(newDist==1) then break end
+            if(newDist<=1) then break end
         end 
 
         table.remove(layersPoints,nnInd)
 
-        print("Moving from "..currentCoords[1]..", "..currentCoords[2].. " to "..nn[1]..", "..nn[2])
+        --print("Moving from "..currentCoords[1]..", "..currentCoords[2].. " to "..nn[1]..", "..nn[2])
         goToCoords(currentCoords,nn)
         currentCoords = nn
 
