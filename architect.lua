2c50f93caeb11bffe0e2bcb4bb2ea6e5dcc07849
Chilly Chili <chillychilichelle@gmail.com>
2024-04-18 18:04:43 +0800

    fixed issue with suboptimal nn movements

--- architect.lua
+++ architect.lua
@@ -132,7 +132,8 @@ for i = 1, y, 1 do
                 nnDist = newDist
                 --print("New nearest neighbour is "..nn[1]..", "..nn[2])
             end
-            if(nnDist<=1) then break end
+
+            if(nnDist==0) then break end
         end 
         table.remove(layersPoints,nnInd)
         
