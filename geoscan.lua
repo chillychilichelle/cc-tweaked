afc7fa928173581e8ee522ba1fabcdd070ac6533
Chilly Chili <chillychilichelle@gmail.com>
2024-04-18 22:48:53 +0800

    fixed bug in geoscan

--- geoscan.lua
+++ geoscan.lua
@@ -5,17 +5,16 @@ local scanner =peripheral.wrap("right");
 local x =3;
 local cost = scanner.cost(x);
 print("It costs "..cost.." fuel to scan with a radius of "..x);
-local result, msg = scanner.scan(5);
+local result, msg = scanner.scan(x);
 
 if result==nil then
     print("Error: "..msg);
 else
     local resultStr = "";
     
-    for key, value in pairs(data) do
-        resultStr=resultStr..value[name].."/\n";
-        resultStr=resultStr.." "..value[x]..","..value[y]..","..value[z].."\n";
-            
-  end
+    for key, value in pairs(result) do
+        resultStr=resultStr..value["name"].."/\n";
+        resultStr=resultStr.." "..value["x"]..","..value["y"]..","..value["z"].."\n";
+    end
     writeToFile(resultStr,"geoOutput.txt");
 end
