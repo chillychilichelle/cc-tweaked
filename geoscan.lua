91003e2ebdc453fd205887c7abedaf0b943d6380
Chilly Chili <chillychilichelle@gmail.com>
2024-04-18 21:39:23 +0800

    debugging scanner

--- geoscan.lua
+++ geoscan.lua
@@ -1,10 +1,11 @@
 package.path = package.path .. ";../?.lua"
 require "utils"
 
+local scanner =peripheral.wrap(right);
 local x =3;
-local cost = cost(x);
-print("It costs "..x.." fuel to scan with a radius of x");
-local result, msg = localscan(5);
+local cost = scanner.cost(x);
+print("It costs "..cost.." fuel to scan with a radius ofx"..x);
+local result, msg = scanner.scan(5);
 
 if result==nil then
     print("Error: "..msg);
