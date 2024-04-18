90225e0e8bada5fe9e202ad285f01303cb52ebcf
Chilly Chili <chillychilichelle@gmail.com>
2024-04-18 17:01:41 +0800

    added stair mining fuel tracking and backtracking

---
+++ stair.lua
@@ -0,0 +1,19 @@
+turtle.refuel()
+local starting = turtle.getFuelLevel()
+local half = starting/2
+local count = 0
+
+while (turtle.detectDown() and turtle.getFuelLevel()>half) do
+    turtle.dig()
+    turtle.digDown()
+    turtle.down()
+    turtle.dig()
+    turtle.forward()
+    count = count+1
+end
+
+for i = count,1,-1
+do
+    turtle.back()
+    turtle.up()
+end
