c55a3bf140b035bc82a66f694600b6f1eb61afdc
Chilly Chili <chillychilichelle@gmail.com>
2024-04-20 16:53:54 +0800

    finalizing QOL changes in geoscan bp and tweaking json block dictionary output

--- geoscanToBlueprint.lua
+++ geoscanToBlueprint.lua
@@ -3,6 +3,12 @@ require "turtleUtils";
 require "utils";
 
 
+local scanner =peripheral.wrap("right");
+if scanner==nil then
+    print("A GEOSCANNER MUST BE EQUIPPED ON THE RIGHT SIDE")
+    return;
+end
+
 print("PLACE IN THE BOTTOM-LEFT CORNER")
 print("(RELATIVE TO NORTH, NOT THE BUILD)")
 print("AT THE LOWEST POINT OF YOUR BUILD")
@@ -12,25 +18,22 @@ local path = "blueprints/"
 local fileName = read()
 fileName= fileName..".txt"
 
+
 if(fs.exists(path..fileName)) then
     print("Overwriting data...")
     fs.delete(path..fileName)
 end
 
+
 print("Select largest axis length/scan radius: ")
 local scanRadius = tonumber(read())
 
 
-local scanner =peripheral.wrap("right");
-local size =2*scanRadius +1;
-
 local result, msg = scanner.scan(scanRadius);
-
 if result==nil then
     print("Error: "..msg);
     return;
 end
-
 
 local mapData ={};
 local blockDictionary ={}
@@ -43,7 +46,8 @@ for i = 1, scanRadius do
     end
 end
 
-local current=65;
+local originalASCII=65;
+local currentASCII=originalASCII;
 
 for _, value in pairs(result) do
     local x = value["x"];
@@ -51,22 +55,22 @@ for _, value in pairs(result) do
     local z = value["z"];
     local name = value["name"];
 
-    if( x<1 or y<1 or z>-1) then
+    if( x<1 or y<0 or z>-1) then
         goto continue
     end
 
     if(blockDictionary[name]==nil)then
-        blockDictionary[name]=string.char(current);
-        current = current + 1
+        blockDictionary[name]=string.char(currentASCII);
+        currentASCII = currentASCII + 1
     end
     mapData[x][y][z]=blockDictionary[name]
     ::continue::
 end
 
 local file = fs.open(path..fileName,"w")
-file.write(size.."\n")
-file.write(size.."\n")
-file.write(size.."\n")
+file.write(scanRadius.."\n")
+file.write(scanRadius.."\n")
+file.write(scanRadius.."\n")
 file.write("\n")
 for i = 0, scanRadius, 1 do
     for j = -1, -scanRadius, -1 do
@@ -82,11 +86,22 @@ for i = 0, scanRadius, 1 do
     file.write("\n")
 end
 
-file.write("\n")
-file.write("{\n")
-for key, value in pairs(blockDictionary) do
-    file.write(" \""..value.."\": \""..key.."\"\n")
+--if any block has been detected
+if currentASCII ~= originalASCII then
+    local c = 1;
+    local cFin = currentASCII-originalASCII;
+    file.write("\n")
+    file.write("{\n")
+    for key, value in pairs(blockDictionary) do
+        if c<cFin then
+            file.write(" \""..value.."\": \""..key.."\",\n")
+        else
+            file.write(" \""..value.."\": \""..key.."\"\n")
+        end
+        c=c+1;
+    end
 end
 file.write("}\n")
+
 
 file.close()
